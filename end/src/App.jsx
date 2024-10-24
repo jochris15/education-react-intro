102821e86b1f5fa2855ff62740f13dbf782e77f8
Joshua Christian <103622276+jochris15@users.noreply.github.com>
2024-10-24 10:57:40 +0700

    Update App.jsx

--- end/src/App.jsx
+++ end/src/App.jsx
@@ -13,13 +13,13 @@ function App() {
         {/* navbar */}
         <nav className="sticky top-0 z-10 p-3 bg-purple-400 border-2 border-black rounded-2xl shadow-[2px_2px_0px_rgba(0,0,0,1)]">
           <div>
+            <a className="text-2xl font-bold px-6 cursor-pointer text-white">
+              <span>Login</span>
+            </a>
             <a className="text-2xl font-bold px-6 cursor-pointer">
               <span>Home</span>
             </a>
-            <a className="text-2xl font-bold px-6 cursor-pointer">
-              <span>Front-end Intro</span>
-            </a>
-            <a className="text-2xl font-bold px-6 cursor-pointer">
+            <a className="text-2xl font-bold px-6 cursor-pointer text-white">
               <span>Logout</span>
             </a>
           </div>
